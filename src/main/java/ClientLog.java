95c8ddfa4cfd770ab93ef5b866366914df59d2c9
jdvpgii <gii.jdvp@gmail.com>
2023-05-30 22:46:33 +0500

    method "ClientLog.log" is done

--- src/main/java/ClientLog.java
+++ src/main/java/ClientLog.java
@@ -1,2 +1,27 @@
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ClientLog {
+    static ArrayList<String> productArray = new ArrayList<>();
+    static ArrayList<String> counterItem = new ArrayList<>();
+    void log(String productNum, String amount) {
+        productArray.add(productNum);
+        counterItem.add(amount);
+    }
+
+    public static void showArray() {
+        System.out.println(productArray);
+        System.out.println(counterItem);
+    }
+
+    void exportAsCVS(File textFile) throws IOException {
+        try (CSVWriter csvWriter = new CSVWriter(new FileWriter(textFile))){
+
+        }
+    }
 }
